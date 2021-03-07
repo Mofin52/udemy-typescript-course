d68102a48b390eeb9bb9d35ce510246b45a5d9b5
Mofin52 <a.velikorechin@gmail.com>
2021-03-07 16:05:55 +0300

    Types intro

---
+++ features/types.ts
@@ -0,0 +1,13 @@
+// Type is an easy way to refer to the different properties and functions that a value has
+const today = new Date();
+today.getMonth();
+
+const person = {
+    age: 20
+}
+console.log(person.age);
+
+class Color {
+
+}
+const red = new Color();
