2eb29a3dbd26c6b943a3ca9feae2ddb6bcac275a
Mofin52 <a.velikorechin@gmail.com>
2021-03-19 23:45:54 +0300

    stats done

---
+++ stats/src/index.ts
@@ -0,0 +1,10 @@
+import path from 'path';
+import { MatchReader } from './MatchReader';
+import { Summary } from './Summary';
+
+const matchReader = MatchReader.fromCsv(path.join('data', 'football.csv'));
+matchReader.load();
+
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
+
+summary.buildAndPrintReport(matchReader.matches);
